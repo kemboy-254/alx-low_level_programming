19c53c1b86fadba1c90b354256e036885eb26df6
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-08-31 23:08:26 +0300

    Print lowercase a;phabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *Prints lowercase alphabet 10 times.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char alpha;
+
+	while (i < 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
