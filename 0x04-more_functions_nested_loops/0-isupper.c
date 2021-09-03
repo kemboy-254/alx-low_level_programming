efddc72a89a12bc72302be0d84baa09ee7d54066
Kemboy <86723520+kemboy-254@users.noreply.github.com>
2021-09-04 01:54:13 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -11,12 +11,12 @@ int _isupper(int c)
         int upper;
 
         if (c >= 65 && c <= 90)
-        {       
+        {
                 upper = 1;
         }       
         else
-        {       
+        {
                 upper = 0;
-        }       
+        }
         return (upper);
 }
