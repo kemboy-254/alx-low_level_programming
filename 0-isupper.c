746e94be4306f039a7af29437380e80fe58105ee
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-09-03 23:49:02 +0300

    Check uppercase

---
+++ 0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: ascii code for the character to check
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int upper;
+
+	if (c <= 90 && c >= 65)
+	{
+		upper = 1;
+	}
+	else
+	{
+		upper = 0;
+	}
+	return (upper);
+}
