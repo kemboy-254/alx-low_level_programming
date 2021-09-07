00181a594e93ef645dad23801d73901c6ecda192
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-09-08 00:44:37 +0300

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: array with the characters of the word
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
