acd41e4fb82d977f2c35578914128915c3a53fe7
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-09-07 19:11:39 +0300

    Swap numbers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as a parameter and update its value
+ * @n: updates the value of the integer it points to
+ *
+ * Return: nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: swaps values with int b in the main code
+ * @b: swaps values with int a in the main code
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
