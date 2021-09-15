bf844ddcafa5a72bbaa12eecbb1d89479b720c29
Kemboy <86723520+kemboy-254@users.noreply.github.com>
2021-09-15 20:04:08 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s - 1);
+		return;
 	}
 
-	else
-		_putchar('\n');
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
