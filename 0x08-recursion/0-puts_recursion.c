bc5251429364d127e9f3d8f2610506ad69d4004a
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-09-15 16:53:56 +0300

    Print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,13 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
                 return;
-        printf("%c", *s);
-        _puts_recursion(s + 1);
-
+	}
+	else
+	{
+		printf("%c", *s);
+		_puts_recursion(s + 1);
+	}
 	_putchar('\n');
 }
