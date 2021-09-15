6b03d15e1bbefb332e7853c200eef5d42a0619b5
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-09-15 14:15:08 +0300

    Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: pointer to the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	printf("%c", *s);
+	_puts_recursion(s + 1);
+}
