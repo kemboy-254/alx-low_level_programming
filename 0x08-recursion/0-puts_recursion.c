96dfd5d810f82de9a6d7d4a0c25fdff18df58579
Kemboy <86723520+kemboy-254@users.noreply.github.com>
2021-09-15 16:36:50 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,20 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                return;
-        }
-        else
-        {
-                printf("%c", *s);
-                _puts_recursion(s + 1);
-        }
-        _putchar('\n');
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
 }
-//	}
-//	else
-//	{
-//		_putchar('\n');
-//	}
-//}
