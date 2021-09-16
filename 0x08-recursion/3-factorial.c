f65ce1d3a522fe33675cd1b8589e9ef812335ed3
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-09-16 23:50:25 +0300

    Print factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,17 +8,17 @@
  */
 int factorial(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    else if (n == 0)
-    {
-        return (1);
-    }
-    else
-    {
-        n = n * factorial(n-1);
-        return n;
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		n = n * factorial(n - 1);
+		return (n);
+	}
 }
