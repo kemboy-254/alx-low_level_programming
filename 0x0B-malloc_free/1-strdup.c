08c4736dcfd329ede28e95b39457c92741e07bc6
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-09-22 17:37:49 +0300

    Duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,24 +8,24 @@
  */
 char *_strdup(char *str)
 {
-	char *p;
-	int cont;
-	int i;
+	char *duplicate;
+	int index, len = 0;
 
-	if (str == '\0')
-		return ('\0');
-	cont = 0;
-	while (str[cont] != '\0')
-	{
-		cont++;
-	}
-	p = malloc((cont * sizeof(char)) + 1);
-	if (p == '\0')
-		return ('\0');
-	for (i = 0; i < cont; i++)
-	{
-		p[i] = str[i];
-	}
-	p[i] = '\0';
-	return (p);
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
