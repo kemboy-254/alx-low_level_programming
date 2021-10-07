ffbd56eaf59921b510bb8ac1201296222cc31d3f
Kemboy <86723520+kemboy-254@users.noreply.github.com>
2021-10-08 01:36:36 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,22 @@
  *
  * Return: the number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t j = 0;
 
-	while (h)
+	while (h != '\0')
 	{
-		if (h->str == NULL)
+		if (h->str == '\0')
+		{
 			printf("[0] (nil)\n");
-
+		}
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		j++;
 		h = h->next;
 	}
-
-	return (nodes);
+	return (j);
 }
