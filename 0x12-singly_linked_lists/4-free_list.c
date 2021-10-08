945df8a9cabc0f2d1f8b342d0bda846403fe6aab
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-10-08 19:29:48 +0300

    Free list_t list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,8 +1,13 @@
 #include "lists.h"
 
+/**
+ * free_list- Frees list_t list
+ * @head: The pointer to node
+ */
 void free_list(list_t *head)
 {
-	struct list_s* tmp;
+	struct list_s *tmp;
+
 	while (head != NULL)
 	{
 		tmp = head;
