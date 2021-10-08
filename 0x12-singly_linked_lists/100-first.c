f8c38a7c469a543b55bfaea44a97c91c5c45dbfc
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-10-08 18:46:23 +0300

    Print before main()

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void before_main(void) __attribute__ ((constructor));
+
+/**
+ * before_main - Prints before the main function
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
