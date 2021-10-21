9183aa2ae04fae71fbda1554705f8d0901bb4751
Michael Kipkemboy <kipkemboymichael05@gmail.com>
2021-10-21 16:23:12 +0300

    Create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, x, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (tect content != NULL)
+	{
+		for ( ; text_content[length];)
+			length++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	x = write(fd, text_content, length);
+
+	if (fd == -1 || x == -1)
+		return (-1);
+
+	close(fd);
+
+	reeturn(1);
+}
